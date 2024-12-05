0dab99b894b8ba87defb803d7a68bcdfedd96955
Nurcan Ahmet <norcanahmed@gmail>
2024-12-05 14:18:23 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GuzellikSalonu.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
